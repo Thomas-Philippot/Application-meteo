9b8ef2044b026bb840d85b91851fd7947459a299
Thomas <thomas.philippot@labo-mylab.fr>
2018-07-26 12:04:17 +0200

    Adding fontawesome icon plugin and side drawer

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,26 +1,52 @@
 <template>
 	<Page class="page">
-		<ActionBar title="Météo" class="action-bar" />
-		<ScrollView>
-			<StackLayout class="home-panel">
-				<StackLayout >
-                    <ActivityIndicator busy="true" @busyChange="processing" :visibility="weather.main ? 'collapsed' : 'visible'"	/>
-					<StackLayout :visibility="weather.main ? 'visible' : 'collapsed'">
-                        <Label class="titre" :text="infos.name" />
-                        <Label class="text" :text="infos.main.temp + ' °C'" />
-                        <Image :src="icon" stretch="aspectFit" class="logo" />
-                        <Label class="text" :text="weather.description" />
+		<ActionBar title="Météo" class="action-bar">
+            <android>
+                <NavigationButton class="fa" :text="'fa-eye'| fonticon"  @tap="onOpenDrawerTap" />
+            </android>
+            <ios>
+                <ActionItem icon="res://ic_menu" ios.position="left" tap="showSideDrawer" />
+            </ios>
+		</ActionBar>
+        <GridLayout rows="*">
+			<RadSideDrawer ref="drawer">
+				<StackLayout ~drawerContent backgroundColor="white">
+					<StackLayout height="56" style="text-align: center; vertical-align: center;">
+						<Label text="Navigation Menu" />
 					</StackLayout>
+					<StackLayout>
+						<Label text="Current Weather" padding="20" backgroundColor="#42b983" />
+						<Label text="Search by city name" padding="20" />
+					</StackLayout>
+					<Label text="Close" color="#42b983" padding="20" style="horizontal-align: center" @tap="onCloseDrawerTap" />
 				</StackLayout>
-			</StackLayout>
-		</ScrollView>
+				<StackLayout ~mainContent class="home-panel">
+                        <ScrollView>
+                            <StackLayout class="home-panel">
+                                <ActivityIndicator busy="true" :visibility="weather.main ? 'collapsed' : 'visible'"	/>
+                                <StackLayout :visibility="weather.main ? 'visible' : 'collapsed'">
+                                    <Label class="titre" :text="infos.name" />
+                                    <Label class="text" :text="infos.main.temp + ' °C'" />
+                                    <Image :src="icon" stretch="aspectFit" class="logo" />
+                                    <Label class="text" :text="weather.description"  />
+                                            <!-- fonticons -->
+                                            <Label class="fa" :text="'fa-bluetooth' | fonticon" />
+                                            <Label class="fa" :text="'fa-heart' | fonticon" />
+                                            <Label class="fa" :text="'fa-user' | fonticon"></Label>
+                                    </StackLayout>
+                        </StackLayout>
+                    </ScrollView>
+				</StackLayout>
+			</RadSideDrawer>
+		</GridLayout>
 	</Page>
 </template>
 
 <script>
-	import { isEnabled, enableLocationRequest, getCurrentLocation, watchLocation, distance, clearWatch } from "nativescript-geolocation";
-  const geoLocation = require("nativescript-geolocation");
-  import axios from 'axios';
+    import { isEnabled, enableLocationRequest, getCurrentLocation, watchLocation, distance, clearWatch } from "nativescript-geolocation";
+    const geoLocation = require("nativescript-geolocation");
+    import axios from 'axios';
+    require("nativescript-vue").registerElement("RadSideDrawer", () => require("nativescript-ui-sidedrawer").RadSideDrawer);
 export default {
     methods: {
         enableLocationServices: function() {
@@ -55,6 +81,12 @@ export default {
                 minimumUpdateTime: 1000 * 1
             });
         },
+        onOpenDrawerTap() {
+            this.$refs.drawer.nativeView.showDrawer();
+        },
+        onCloseDrawerTap() {
+            this.$refs.drawer.nativeView.closeDrawer();
+        },
     },
     created: function() {
       this.enableLocationServices();
@@ -74,16 +106,22 @@ export default {
             key: "b6907d289e10d714a6e88b30761fae22",
             url: "https://openweathermap.org/data/2.5/weather",
             icon: "",
+            mainContentText: "SideDrawer for NativeScript can be easily setup in the XML definition of your page by defining main- and drawer-content. The component" +
+                " has a default transition and position and also exposes notifications related to changes in its state. Swipe from left to open side drawer.",
         };
     },
 }
 </script>
 
 <style scoped>
-	.home-panel {
+    .page {
+        background-color: white;
+    }
+    .home-panel {
 		font-size: 20;
 		margin: 15;
         vertical-align: center;
+        align-items: center;
 	}
 	.description-label {
 		margin-bottom: 15;
@@ -96,7 +134,7 @@ export default {
     .titre {
         text-align: center;
         color: #42b983;
-        font-size: 50px;
+        font-size: 40%;
     }
     .logo {
         display: block;
@@ -106,5 +144,10 @@ export default {
         margin-bottom: 20px;
         width: 200px;
         height: 200px;
+    }
+    .button {
+        width: 50%;
+        background-color: #3e5bfe;
+        color: white;
     }
 </style>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,16 @@ import HelloWorld from './components/HelloWorld';
 
 import './styles.scss';
 
+import {TNSFontIcon, fonticon} from 'nativescript-fonticon';
+
+TNSFontIcon.debug = true;
+TNSFontIcon.paths = {
+  'fa': './font-awesome.css',
+};
+TNSFontIcon.loadCss();
+
+Vue.filter('fonticon', fonticon);
+
 // Uncomment the following to see NativeScript-Vue output logs
 //Vue.config.silent = false;
 
